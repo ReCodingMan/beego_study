19391fa4b55e62f2d7db26fd9b46874c97b80931
chengcheng1021 <chengcheng1021@aliyun.com>
2020-12-15 15:25:25 +0800

    两种md5方法

--- beegodemo02/controllers/cms.go
+++ beegodemo02/controllers/cms.go
@@ -2,7 +2,10 @@ package controllers
 
 import (
 	"beegodemo02/models"
+	"crypto/md5"
+	"fmt"
 	"github.com/astaxie/beego"
+	"io"
 )
 
 type CmsController struct {
@@ -20,6 +23,14 @@ func (c *CmsController) Cms() {
 }
 
 func (c *CmsController) ModelFunc() {
+	//md5加密
+	h := md5.New()
+	io.WriteString(h, "123456") //e10adc3949ba59abbe56e057f20f883e
+	fmt.Printf("%x\n", h.Sum(nil))
+
+	date := []byte("123456")
+	fmt.Printf("%x\n", md5.Sum(date))
+
 	str := models.UnixToDate(1589898013)
 	c.Ctx.WriteString("新闻add" + str)
 }
